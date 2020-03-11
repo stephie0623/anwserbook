e0932d0abdea8e48e37eff7aae0217034729b8d2
stephie0623 <49089659+stephie0623@users.noreply.github.com>
2020-03-11 14:33:03 +0800

    2

--- anwser.js
+++ anwser.js
@@ -1,6 +1,6 @@
  function Appear() {
      //获取随机数
-     var luckyNum = Math.round(Math.random() * 4);
+     var luckyNum = Math.round(Math.random() * 22);
      //alert(luckyNum);
      if (luckyNum == 0) {
          window.location.href = "./sol/sol1.html";
@@ -10,5 +10,41 @@
          window.location.href = "./sol/sol3.html";
      } else if (luckyNum == 3) {
          window.location.href = "./sol/sol4.html";
+     } else if (luckyNum == 4) {
+         window.location.href = "./sol/sol5.html";
+     } else if (luckyNum == 5) {
+         window.location.href = "./sol/sol6.html";
+     } else if (luckyNum == 6) {
+         window.location.href = "./sol/sol7.html";
+     } else if (luckyNum == 7) {
+         window.location.href = "./sol/sol8.html";
+     } else if (luckyNum == 8) {
+         window.location.href = "./sol/sol9.html";
+     } else if (luckyNum == 9) {
+         window.location.href = "./sol/sol10.html";
+     } else if (luckyNum == 10) {
+         window.location.href = "./sol/sol11.html";
+     } else if (luckyNum == 11) {
+         window.location.href = "./sol/sol12.html";
+     } else if (luckyNum == 12) {
+         window.location.href = "./sol/sol13.html";
+     } else if (luckyNum == 13) {
+         window.location.href = "./sol/sol14.html";
+     } else if (luckyNum == 14) {
+         window.location.href = "./sol/sol15.html";
+     } else if (luckyNum == 15) {
+         window.location.href = "./sol/sol16.html";
+     } else if (luckyNum == 16) {
+         window.location.href = "./sol/sol17.html";
+     } else if (luckyNum == 17) {
+         window.location.href = "./sol/sol18.html";
+     } else if (luckyNum == 18) {
+         window.location.href = "./sol/sol19.html";
+     } else if (luckyNum == 19) {
+         window.location.href = "./sol/sol20.html";
+     } else if (luckyNum == 20) {
+         window.location.href = "./sol/sol21.html";
+     } else if (luckyNum == 21) {
+         window.location.href = "./sol/sol22.html";
      }
  }
